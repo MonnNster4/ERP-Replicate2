cc2bc50af45142900ea46f64d43d473a0a1acfca
MonnNster4 <monsantiago09@gmail.com>
2023-11-24 23:18:09 +0800

    design

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,29 @@
 module.exports = {
     content: ["./src/**/*.{html,js}"],
     theme: {
-      extend: {},
+      extend: {
+        fontFamily: {
+          primary: ['Red Hat Display', "sans-serif"],
+          secondary: ['Roboto', 'sans-serif']
+        },
+        textColor: {
+          primaryBlack: "#22092C",
+          primaryRed: "#C70039",
+          primaryGray: "#4B5563"
+        },
+        backgroundColor: {
+          primary: "#22092C",
+          secondary: "#C70039",
+          tertiary: "#9D9D9D",
+          hover: "#4caf4f",
+          mainWhite: "#fff",
+          screen: "#eceff180"
+        },
+        borderColor: {
+          primaryBlack: "#22092C",
+          primaryRed: "#C70039",
+        }
+      },
     },
     plugins: [],
   }
